21a6ee6ccb7bffb8d8d4ab8be04eaf9473678b98
KiddoKenshin <kiddokenshin@hotmail.com>
2016-10-21 18:02:17 +0900

    Typo. Used backtick as single quotation.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array(‘database’);
+$autoload['libraries'] = array('database');
 
 /*
 | -------------------------------------------------------------------
